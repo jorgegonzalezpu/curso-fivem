af0bae58ca1f094d8316f9df5fa598248590e9e1
esFer <58149189+esFer102@users.noreply.github.com>
2021-03-03 19:25:52 +0100

    Add files via upload

---
+++ 1· Cosas básicas.lua
@@ -0,0 +1,86 @@
+-- Esto es un comentario :D
+
+-- Esto sería otro comentario :D
+Esto ya no :p, esto ya es código y te lo daría como error.
+-- Los comentarios no te lo dan como error ya que no los mira.
+
+--[[
+ESTO ES UN
+COMENTARIO
+MUUUUUY LARGO,
+
+Y PARA TERMINAR ESTA COMENTARIO,
+COMO TODO, HAY QUE CERRARLO CON: ]]
+Y aqui cierra el comentario (como dijimos antes, esto lo daría como error ya que esto no es nada)
+
+--------------------------------------------
+
+-- ################################
+-- COMO DECLARAR UNA VARIABLE
+-- ################################
+
+local dinero = 1 -- Un eurito
+--[[
+
+"local" hace declarar a la variable
+"dinero" sería el nombre de la variable (el cual es totalmente custom)
+"1" sería la cantidad de la variable
+
+Ah, por cierto, te recomiendo no usar nunca la ñ en el cógido :p
+--]]
+
+-- ################################
+-- TIPOS DE VARIABLE
+-- ################################
+
+-- BOOL (Para poner si es true (verdadero) o false (falso))
+local dinero = true
+
+-- NUMBER (Para poner números como en el primer ejemplo de como declarar una variable)
+local dinero = 10 -- Dieh eurilloz :p
+local edad = 76
+local precio = 5
+
+-- STRING (Un texto que siempre debe estar rodeado de comillas)
+local nombre = "Augusta"
+local edad = "72" -- Esto NO es un número (NUMBER) porqué está rodeado de comillas.
+local AugustaTrabajando = "false" -- Esto NO es un bool, porqué está rodeado de comillas.
+
+-- Un ejemplo de cosas para comprar:
+local cosasParaComprar = {
+	"lechexd",
+	"abuelos",
+	"niños para el sótano"
+}
+
+-- Ordenar las variables:
+local persona = {nombre="Darkyzz", gordo="true", edad="69"}
+
+local personaORDENADA = {
+	nombre = "Darkyzz",
+	gordo = "true",
+	edad = "69"
+}
+
+-- ###############################################################################
+-- Bien, por hoy ha sido suficiente, ahora, haz unos ejercicios de deberes:
+-- ###############################################################################
+
+--[[
+
+	EJERCICIO
+
+	1. Tu nombre (nombre)
+	2. Año en el que naciste (ano)
+	3. Tu edad (edad)
+	4. El nombre de un amigo (nombreAmigo)
+	5. Cosas que tienes que comprar (cosasParaComprar)
+
+--]]
+
+-- Voy a ayudate un poco...
+
+local nombre = "esFerDev"
+local ano = 1254 -- si, pone ano porque como dije antes, era mejor no poner ñ en el código.
+
+-- Ahora sigue :D
